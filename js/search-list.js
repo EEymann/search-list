80c0d126f53d5ef99dc0515a86528d5f803a5b3a
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-09-04 11:23:49 -0700

    write an else if statement using join(), includes(), and indexOf() to iterate over an array in search of an item entered by the user.

--- js/search-list.js
+++ js/search-list.js
@@ -1,3 +1,14 @@
 const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
 const search = prompt('Search for a product.');
 let message;
+
+/** Use the logical not operator (!) in the condition to check if the value
+assigned to the search variable is falsy, such as a blank entry. **/
+
+if ( !search ) {
+  message = `<strong>In stock:</strong> ${inStock.join(', ')}`;
+} else if ( inStock.includes(search.toLowerCase())) { // toLowerCase ensures statement evaluates to true even if the user types capital letters.
+  message = `Yes, we have <strong>${search}</strong>. It's #${inStock.indexOf(search.toLocaleLowerCase()) + 1 } on the list!`;
+} else {
+  message = `Sorry, we do not have <strong>${search}</strong>.`
+}
